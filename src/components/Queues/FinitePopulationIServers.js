e87870a91bb0253e3f4254d6c403dc8394de354a
Diogo Mafra <diogomqbm13@gmail.com>
2020-11-18 19:03:16 -0300

    finite population infinite servers

--- src/components/Queues/FinitePopulationIServers.js
+++ src/components/Queues/FinitePopulationIServers.js
@@ -1,11 +1,51 @@
 import * as React from 'react';
-import { Box, Grid, Heading, Input, Label, Slider, Text } from 'theme-ui';
+import { Box, Card, Grid, Heading, Input, Label, Slider, Text } from 'theme-ui';
+
+function nRequisitions(user, population, requisitions, processing) {
+  if (user <= population) {
+    return (Math.pow((requisitions / processing), user) *
+      (population / user)) /
+      Math.pow(1.0 + (requisitions / processing), population);
+  }
+  else {
+    return 0.0;
+  }
+}
+
+function noRequisitions(requisitions, processing, population) {
+
+  var result = 1.0 + (requisitions / processing);
+  return 1.0 / (Math.pow(result, population));
+}
+
+function ro(population, requisitions, processing) {
+  var result = (population * requisitions) / (1 + requisitions / processing);
+
+  if (result >= 1.0) {
+    return 1.0;
+  } else if (result <= 0.0) {
+    return 0.0;
+  }
+  return result;
+}
+
+function avgUser(population, requisitions, processing) {
+  var result = (population * (requisitions / processing));
+
+  return result / (1.0 + requisitions / processing);
+}
+
 
 function FinitePopulationIServers() {
   const [processing, setProcessing] = React.useState(0);
   const [requisitions, setRequisitions] = React.useState(0);
-  const [servers, setServers] = React.useState(1);
+  const [population, setPopulation] = React.useState(1);
   const [user, setUser] = React.useState(0);
+
+  const trafficIntensity = ro(population, requisitions, processing);
+  const noReq = noRequisitions(requisitions, processing, population);
+  const nReq = nRequisitions(user, population, requisitions, processing);
+  const userAvg = avgUser(population, requisitions, processing);
 
   return (
     <Box p={[2, 2]}>
@@ -34,10 +74,35 @@ function FinitePopulationIServers() {
             />
           </Box>
         </Grid>
+        <Box mb={3}>
+          <Label htmlFor="user">Usuários</Label>
+          <Input
+            value={user}
+            onChange={e => setUser(e.target.value)}
+            type="number"
+            min="0"
+            name="user"
+          />
+        </Box>
         <Label>População</Label>
-        <Text p={2} sx={{ fontSize: 4 }}>{servers}</Text>
-        <Slider value={servers} onChange={(e) => setServers(e.target.value)} step={1} />
+        <Text p={2} sx={{ fontSize: 4 }}>{population}</Text>
+        <Slider value={population} onChange={(e) => setPopulation(e.target.value)} step={1} />
       </Box>
+      <Card sx={{
+        borderRadius: '8px',
+        padding: '10px',
+        border: '1px solid #ccc',
+        boxShadow: '0 0 10px #ccc',
+      }}>
+        <Heading>Ro</Heading>
+        <Heading as="h3">{(100 * trafficIntensity).toFixed(2)}%</Heading>
+        <Heading>Probabilidade de nenhum usuário no sistema</Heading>
+        <Heading as="h3">{(100 * noReq).toFixed(2)}%</Heading>
+        <Heading>Probabilidade de n usuários no sistema</Heading>
+        <Heading as="h3">{(100 * nReq).toFixed(2)}%</Heading>
+        <Heading>Qtd média de usuários no sistema</Heading>
+        <Heading as="h3">{userAvg.toFixed(2)}%</Heading>
+      </Card>
     </Box>
   )
 }
